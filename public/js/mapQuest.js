a5c667f327db4ec078751112e8b8e44f6eb2ba72
jil1064 <jil1064@ucsd.edu>
2020-02-26 11:29:55 -0800

    lab 8

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,13 @@
 function initMap() {
 	// add your code here
+    L.mapquest.key = '	S0GItkkyrnlApxLIOmZnDnaXqhh9Fd7c';
+
+    // 'map' refers to a <div> element with the ID map
+    var map = L.mapquest.map('map', {
+      center: [32.88, -117.236],
+      layers: L.mapquest.tileLayer('map'),
+      zoom: 14
+    });
+    
+    L.marker([32.88, -117.236]).addTo(map);
 }
